f432890e973db35b8ab0bff555050fdcf92ee1f4
ItsLucas <itslucas@itslucas.me>
2015-07-28 19:49:41 +0800

    Initial Commit

---
+++ CrazyPackager/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CrazyPackager
+//
+//  Created by Lucas on 7/28/15.
+//  Copyright © 2015 CrazyMCP. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
